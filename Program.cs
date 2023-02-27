6d4b321fce6b9336b75bc9f1eec6ba7c1ecd4469
Sávio <saviopf22@gmail.com>
2023-02-26 23:55:08 -0300

    init c# learning

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+bool b;
+char c;
+byte by;
+int i;
+long l;
+float f;
+double d;
+decimal de;
+string s;
+
+int i2 = i = 10;
+
+int i3 = --i2;
+
+c = 'o';
+
+s = "test";
+
+Console.WriteLine(i3 != i ? true : false);
+
+// https://www.youtube.com/watch?v=oTivhgjbhIg
+// 26:25 - tipagem estática
